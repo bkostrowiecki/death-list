8f8c98080e042376f31d95d3d3bec6369d34b6b6
Bartosz Kostrowiecki <bartosz.kostrowiecki@softwareone.com>
2019-11-27 21:12:50 +0100

    basic styling

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DeathList } from './DeathList/DeathList';
 
 class App extends React.Component {
   render() {
-    return <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-6">
+          <DeathList />
+        </div>
+      </div>
+    </div>;
   }
 }
 
--- src/App/DeathList/DeathList.tsx
+++ src/App/DeathList/DeathList.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 export class DeathList extends React.Component<{}> {
   render() {
-    return <div class="form-group">
-      <label class="form-label" for="personName">Person name</label>
-      <input class="form-input" type="text" id="personName" />
+    return <div className="form-group">
+      <label className="form-label" htmlFor="personName">Person name</label>
+      <div className="input-group">
+        <input className="form-control" type="text" id="personName" />
+        <div className="input-group-append">
+          <button className="btn btn-outline-secondary" type="button">Add person</button>
+        </div>
+      </div>
     </div>;
   }
 }
