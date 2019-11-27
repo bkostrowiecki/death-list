f45d38ce4625bbd258e9095f35a2df19136d8fd6
Bartosz Kostrowiecki <bartosz.kostrowiecki@softwareone.com>
2019-11-27 21:33:05 +0100

    pitfall 2

--- src/App/DeathList/DeathList.tsx
+++ src/App/DeathList/DeathList.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { SyntheticEvent, ChangeEvent } from "react";
 
-export class DeathList extends React.Component<{}> {
+export class DeathList extends React.Component<{}, DeathListState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      personName: ''
+    };
+  }
+
+  onPersonNameChange(event: ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      personName: event.target.value
+    });
+
+    console.log(this.state.personName);
+  }
+
   render() {
     return <div className="form-group">
       <label className="form-label" htmlFor="personName">Person name</label>
       <div className="input-group">
-        <input className="form-control" type="text" id="personName" />
+        <input className="form-control" type="text" id="personName" value={this.state.personName} onChange={this.onPersonNameChange} />
         <div className="input-group-append">
           <button className="btn btn-outline-secondary" type="button">Add person</button>
         </div>
       </div>
     </div>;
   }
+}
+
+interface DeathListState {
+  personName: string;
 }
