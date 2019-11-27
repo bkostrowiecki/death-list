41941a7f5cdae5c9fa10751ff2e85ca21007f6bb
Bartosz Kostrowiecki <bartosz.kostrowiecki@softwareone.com>
2019-11-27 20:38:32 +0100

    change App component to be class component instead of fc

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
+class App extends React.Component {
+  render() {
+    return <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,7 +20,7 @@ const App: React.FC = () => {
         </a>
       </header>
     </div>
-  );
+  }
 }
 
 export default App;
