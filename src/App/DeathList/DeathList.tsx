403f4f08ab0d9d6d1ce3c75ea8ab544ee9e75e78
Bartosz Kostrowiecki <bartosz.kostrowiecki@softwareone.com>
2019-11-27 22:08:05 +0100

    pitfoll 4

--- src/App/DeathList/DeathList.tsx
+++ src/App/DeathList/DeathList.tsx
@@ -1,37 +1,69 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, FormEvent } from "react";
 
 export class DeathList extends React.Component<{}, DeathListState> {
   constructor(props: {}) {
     super(props);
 
     this.state = {
-      personName: ''
+      personName: '',
+      people: []
     };
 
     this.onPersonNameChange = this.onPersonNameChange.bind(this);
+    this.onAddPersonSubmit = this.onAddPersonSubmit.bind(this);
   }
 
   onPersonNameChange(event: ChangeEvent<HTMLInputElement>) {
     this.setState({
       personName: event.target.value
-    }, () => {
-      console.log(this.state.personName);
     });
   }
 
+  onAddPersonSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    this.setState({
+      people: [
+        this.state.personName,
+        ...this.state.people
+      ],
+      personName: ''
+    });
+  }
+
+  onDeletePersonClick(indexToDelete: number) {
+    return () => {
+      this.setState({
+        people: [
+          ...this.state.people.filter((_item, index) => index !== indexToDelete)
+        ]
+      });
+    };
+  }
+
   render() {
-    return <div className="form-group">
+    return <>
+    <form className="form-group" onSubmit={this.onAddPersonSubmit}>
       <label className="form-label" htmlFor="personName">Person name</label>
       <div className="input-group">
         <input className="form-control" type="text" id="personName" value={this.state.personName} onChange={this.onPersonNameChange} />
         <div className="input-group-append">
-          <button className="btn btn-outline-secondary" type="button">Add person</button>
+          <button className="btn btn-outline-secondary" type="submit">Add person</button>
         </div>
       </div>
-    </div>;
+    </form>
+    {this.renderList()}
+    </>;
+  }
+
+  private renderList() {
+    return <ul className="list-group list-group-flush">
+      {this.state.people.map((item, index) => <li className="list-group-item" key={index}>{item} <button type="button" onClick={this.onDeletePersonClick(index)}>Delete</button></li>)}
+    </ul>;
   }
 }
 
 interface DeathListState {
   personName: string;
+  people: string[];
 }
